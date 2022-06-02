c84ba3137019d8f951b5531d0fd1c68a4e17a5a2
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2022-06-02 19:03:29 +0000

    adds meta theme-color

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,17 @@
 import Title from '../components/Title'
 import Ingress from '../components/Ingress'
+import Head from 'next/head'
 
 export default function Home() {
   return (
   <>
+    <Head>
+      <title>Next with Styled Components Template</title>
+      <meta name="HandheldFriendly" content="true" />
+      <meta name="theme-color" content="#fafafa" media="(prefers-color-scheme: light)"/>
+      <meta name="theme-color" content="#1e2024" media="(prefers-color-scheme: dark)" />
+    </Head>
+  
     <Title>Hello?</Title>
     <Ingress>Yo!</Ingress>
     <p>What?!</p>
